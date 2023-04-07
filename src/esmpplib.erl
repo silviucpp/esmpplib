e3c1eedb101a4c2d08d30faa9e32e76c8e26df12
silviu <silviu.cpp@gmail.com>
2023-04-07 23:37:19 +0000

    Fix type specs

--- src/esmpplib.erl
+++ src/esmpplib.erl
@@ -44,19 +44,28 @@ restart_pool(PoolName) ->
     erlpool:restart_pool(PoolName).
 
 -spec submit_sm(atom(), binary(), binary(), binary()) ->
-    ok | {error, reason()}.
+    {ok, MessageId::binary(), Parts::non_neg_integer()} | {error, reason()}.
 
 submit_sm(PoolName, SrcAddr, DstAddr, Message) ->
     Pid = erlpool:pid(PoolName),
     esmpplib_connection:submit_sm(Pid, SrcAddr, DstAddr, Message).
 
+-spec submit_sm_async(atom(), any(), binary(), binary(), binary()) ->
+    ok | {error, reason()}.
+
 submit_sm_async(PoolName, MessageRef, SrcAddr, DstAddr, Message) ->
     Pid = erlpool:pid(PoolName),
     esmpplib_connection:submit_sm_async(Pid, MessageRef, SrcAddr, DstAddr, Message).
 
+-spec query_sm(atom(), binary()) ->
+    {ok, Resp::[{atom(), any()}]} | {error, reason()}.
+
 query_sm(PoolName, MessageId) ->
     Pid = erlpool:pid(PoolName),
     esmpplib_connection:query_sm(Pid, MessageId).
+
+-spec query_sm_async(atom(), binary()) ->
+    ok | {error, reason()}.
 
 query_sm_async(PoolName, MessageId) ->
     Pid = erlpool:pid(PoolName),
