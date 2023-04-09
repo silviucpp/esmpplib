40e0eb3ffc5a620c8d65a487ad747b8c1895ef06
silviu <silviu.cpp@gmail.com>
2023-04-09 07:49:39 +0000

    Remove unused methods

--- src/esmpplib_time.erl
+++ src/esmpplib_time.erl
@@ -2,23 +2,11 @@
 
 -export([
     now_msec/0,
-    now_sec/0,
-    now_date_time/0,
-    datetime2ts/1,
-    ts2datetime/1
+    datetime2ts/1
 ]).
 
 now_msec() ->
     tic:now_to_epoch_msecs().
 
-now_sec() ->
-    tic:now_to_epoch_secs().
-
-now_date_time() ->
-    tic:epoch_secs_to_datetime(now_sec()).
-
 datetime2ts(DateTime) ->
     tic:datetime_to_epoch_secs(DateTime).
-
-ts2datetime(Ts) ->
-    tic:epoch_secs_to_datetime(Ts).
